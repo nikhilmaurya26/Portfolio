3e8767096fe7da50308750e6faaa216d8e17d1ed
Nikhil <nikhilsingh0926@gmail.com>
2023-07-20 12:29:45 +0530

    Subject:Fixed portfolio link

--- src/Components/menu/Menu.jsx
+++ src/Components/menu/Menu.jsx
@@ -9,7 +9,7 @@ export default function Menu({menuOpen,setMenuOpen}) {
                 < a href='#intro'>Home</a>
             </li>
             <li onClick={()=>setMenuOpen(false)}>
-                < a href='portfolio'>portfolio</a>
+                < a href='#portfolio'>portfolio</a>
             </li>
             <li onClick={()=>setMenuOpen(false)}>
                 < a href='#works'>Work</a>
